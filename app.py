2d92e826d279d890aba68a4d91c0b9e7513f20de
Anurag3215 <anuragaacc@gmail.com>
2026-07-02 20:55:07 +0530

    Update app to use environment variable for port
    Change the app to use environment variable for port and disable debug mode.

--- app.py
+++ app.py
@@ -314,6 +314,7 @@ def init_db():
             print("Default admin created -> email: admin@example.com | password: admin123")
 
 
+
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
